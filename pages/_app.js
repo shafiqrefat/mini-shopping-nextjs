d4ea9bfd8cffa8ac839be3bfb0b5c56723f3ff0d
shafiq_refat <shafiqur@amharctech.com>
2023-05-18 21:33:03 +0600

    favicon & page title changed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import store from '@/features/store';
 import '@/styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
@@ -9,6 +10,9 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
   return <Provider store={store}>
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Mini Shop</title>
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </QueryClientProvider>
